2ff6b9d5eb4b309f8076ccef75b23950cd582435
Shail Patel <shailpatel1996@gmail.com>
2023-10-03 19:27:33 -0400

    Added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -44,6 +46,60 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                color: Colors.purple,
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        log('Button Pressed');
+                      },
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.orange),
+                        child: const Icon(Icons.person_2_rounded),
+                      ),
+                    ),
+                    const Text('email@gmai..com'),
+                    const Text('name'),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  ...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+                      .map(
+                        (e) => ListTile(
+                          title: Text('Item $e'),
+                          onTap: () {
+                            // Update the state of the app.
+                            // ...
+                          },
+                        ),
+                      )
+                      .toList(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
       body: Center(
         child: Column(
